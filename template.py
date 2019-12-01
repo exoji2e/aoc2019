ca38a7324477d9f3f9fa2d061fa63e2a8cd158c3
Måns Magnusson <exoji2e@gmail.com>
2019-12-01 21:16:57 +0100

    cleanup template

--- template.py
+++ template.py
@@ -1,19 +1,19 @@
+#!/usr/bin/python3
 import sys, time
 sys.path.extend(['..', '.'])
 from collections import *
 from main import run
+def get_day(): return 1
+def get_year(): return 2019
 
-def p1(v, log=False):
+def p1(v):
+    lines = v.strip().split('\n')
     return 0
 
-def p2(v, log=False):
+def p2(v):
+    lines = v.strip().split('\n')
     return 0
 
-def get_day():
-    return 1
-
-def get_year():
-    return 2019
 
 if __name__ == '__main__':
     run(get_year(), get_day(), p1, p2, D=True)
